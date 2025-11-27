7c45f1c9da80232aecb3e2f812b70974cbc24afb
Kaua Santos <kavillykaua@gmail.com>
2025-11-27 17:41:27 -0300

    fix: adciona imports das bibliotecas `json` e `os` para manipular arquivos e corrigir erro

---
+++ gerenciar_arquivos.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+base_dir = os.path.dirname(__file__)
+save_to = os.path.join(base_dir, 'historico.json')
+
+def arquivo_r():
+    try:
+        with open(save_to, "r", encoding="utf-8") as file:
+            return json.load(file)
+    except json.JSONDecodeError:
+        return
+
+def arquivo_w(historico):
+    with open(save_to, "w", encoding="utf-8") as file:
+        json.dump(historico, file, indent=2, ensure_ascii=False)
