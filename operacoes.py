42a45d906d03a755453b0f8550d3a8633ae3b763
Kaua Santos <kavillykaua@gmail.com>
2025-11-27 20:27:18 -0300

    refactor: renomeia a lista que agrupa os números informados pelo usuário para melhor compreensão e legibilidade
    - lista -> lista_numeros

--- operacoes.py
+++ operacoes.py
@@ -1,20 +1,20 @@
-def soma(lista):
+def soma(lista_numeros):
     soma = 0
-    for numero in lista:
+    for numero in lista_numeros:
         soma += numero
     return soma
 
-def subtracao(lista):
-    resultado = lista[0]
-    for i in range(1, len(lista)):
-        resultado -= lista[i]
+def subtracao(lista_numeros):
+    resultado = lista_numeros[0]
+    for i in range(1, len(lista_numeros)):
+        resultado -= lista_numeros[i]
     return resultado
 
 def divisao(a, b):
     return a/b
 
-def multiplicacao(lista):
+def multiplicacao(lista_numeros):
     mult = 1
-    for numero in lista:
+    for numero in lista_numeros:
         mult *= numero
     return mult
