2c6f9e5446961a221386aefa3a028539a7735e72
Kaua Santos <kavillykaua@gmail.com>
2025-11-28 13:53:43 -0300

    refactor: simplifica a lógica de validação de números em uma única função
    - Utiliza uma função única para validar números do tipo int ou float
    - Remove lógica duplicada de validação

--- validadores.py
+++ validadores.py
@@ -1,13 +1,9 @@
-def validar_int(numero):
+def validar_numero(numero):
     try:
-        numero = int(numero)
-        return numero
-    except ValueError:
-        raise ValueError("O número não deve conter letras, espaços ou caracteres especiais.")
-
-def validar_float(numero):
-    try:
-        numero = float(numero)
+        if numero.isdigit():
+            numero = int(numero)
+        else:
+            numero = float(numero)
         return numero
     except ValueError:
         raise ValueError("O número não deve conter letras, espaços ou caracteres especiais.")
