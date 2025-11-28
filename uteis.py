44be0b95ddd7a2cdf26bea8effd2453f3c4a5cdf
Kaua Santos <kavillykaua@gmail.com>
2025-11-28 14:37:01 -0300

    refactor: reorganiza lógica que verifica a quantidade de números informados pelo usuário na função solicitar_dados()
    - Utiliza if para verificar se a quantidade de números informados é inferior a dois
    - Levanta um ValueError caso a condição seja verdadeira
    - Retorna a lista de números caso a condição seja falsa

--- uteis.py
+++ uteis.py
@@ -21,10 +21,15 @@ def menu_opcoes():
     print("\n[1]- Soma\n[2]- Subtração\n[3]- Divisão\n[4]- Multiplicação\n[5]- Histórico de operações\n[6]- Apagar histórico\n[7]- Sair\n")
 
 def solicitar_numeros():
-     lista_numeros = []
-     while True:
+    lista_numeros = []
+    while True:
         numero = input("Informe dois números ou mais (digite 'sair' para finalizar): ")
         if numero.lower() == 'sair':
-            return lista_numeros
+            break
         numero = validadores.validar_numero(numero)
         lista_numeros.append(numero)
+    
+    if len(lista_numeros) < 2:
+        raise ValueError("Quantidade de números insuficientes para realizar a operação.\n")
+    
+    return lista_numeros
