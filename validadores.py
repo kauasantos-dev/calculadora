a10ed85e9364635eaa1903058e6e9f9bd2adcebf
Kaua Santos <kavillykaua@gmail.com>
2025-12-01 14:13:51 -0300

    style: adiciona quebra de linha na mensagem de erro de validar_numero

--- validadores.py
+++ validadores.py
@@ -6,7 +6,7 @@ def validar_numero(numero):
             numero = float(numero)
         return numero
     except ValueError:
-        raise ValueError("O número não deve conter letras, espaços ou caracteres especiais.")
+        raise ValueError("O número não deve conter letras, espaços ou caracteres especiais.\n")
 
 def validar_divisor(divisor):
     if divisor == 0:
