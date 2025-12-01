33c8564971dd639387fbce9ce8248344b9820754
Kaua Santos <kavillykaua@gmail.com>
2025-12-01 13:59:24 -0300

    refactor: ajusta a ordem das funções para melhor compreensão e legibilidade do programa

--- uteis.py
+++ uteis.py
@@ -1,6 +1,19 @@
 import gerenciar_arquivos
 import validadores
 
+def ver_historico():
+    historico = gerenciar_arquivos.arquivo_r()
+    if not historico:
+        print("O HISTÓRICO DE OPERAÇÕES ESTÁ VAZIO.\n")
+    else:
+        print("HISTÓRICO DE OPERAÇÕES:")
+        for produto in historico:
+            for chave, valor in produto.items():
+                print(f"{chave} = {valor}")
+
+def apagar_historico():
+    gerenciar_arquivos.arquivo_w([])
+
 def salvar_historico(nova_operação_efetuada):
     historico_salvo = gerenciar_arquivos.arquivo_r()
     if not historico_salvo:
@@ -8,6 +21,14 @@ def salvar_historico(nova_operação_efetuada):
     else:
         historico_salvo.append(nova_operação_efetuada)
     gerenciar_arquivos.arquivo_w(historico_salvo)
+
+def historico_nova_operacao(lista_numeros, operação, resultado):
+    for valor in range(len(lista_numeros)):
+        lista_numeros[valor] = str(lista_numeros[valor])
+
+    operacao_realizada = f" {operação} ".join(lista_numeros)
+
+    return {operacao_realizada: resultado}
 
 def menu_opcoes():
     print("\n[1]- Soma\n[2]- Subtração\n[3]- Divisão\n[4]- Multiplicação\n[5]- Ver histórico de operações\n[6]- Apagar histórico\n[7]- Sair\n")
@@ -32,32 +53,6 @@ def solicitar_numeros(simbolo_da_operacao):
     
     return lista_numeros
 
-def historico_nova_operacao(lista_numeros, operação, resultado):
-    for valor in range(len(lista_numeros)):
-        lista_numeros[valor] = str(lista_numeros[valor])
-
-    operacao_realizada = f" {operação} ".join(lista_numeros)
-
-    return {operacao_realizada: resultado}
-
-def exibir_resultado_operacao(operacao_realizada):
-    operacao = list(operacao_realizada.keys())
-    resultado = list(operacao_realizada.values())
-    print(f"\n{operacao[0]} = {resultado[0]}")
-
-def ver_historico():
-    historico = gerenciar_arquivos.arquivo_r()
-    if not historico:
-        print("O HISTÓRICO DE OPERAÇÕES ESTÁ VAZIO.\n")
-    else:
-        print("HISTÓRICO DE OPERAÇÕES:")
-        for produto in historico:
-            for chave, valor in produto.items():
-                print(f"{chave} = {valor}")
-
-def apagar_historico():
-    gerenciar_arquivos.arquivo_w([])
-
 def realizar_operacao(operacao_selecionada, simbolo_da_operacao):
     try:
         lista_numeros = solicitar_numeros(simbolo_da_operacao)
@@ -66,3 +61,8 @@ def realizar_operacao(operacao_selecionada, simbolo_da_operacao):
     resultado = operacao_selecionada(lista_numeros)
     exibir_resultado_operacao(historico_nova_operacao(lista_numeros, simbolo_da_operacao, resultado))
     salvar_historico(historico_nova_operacao(lista_numeros, simbolo_da_operacao, resultado))
+    
+def exibir_resultado_operacao(operacao_realizada):
+    operacao = list(operacao_realizada.keys())
+    resultado = list(operacao_realizada.values())
+    print(f"\n{operacao[0]} = {resultado[0]}")
