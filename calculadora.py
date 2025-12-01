84e44ea2376d85602522391ceb220d0d5ea18d18
Kaua Santos <kavillykaua@gmail.com>
2025-12-01 10:46:48 -0300

    refactor: reorganiza chamada de funções em uma função única para efetuar e exibir operações nas opções 1-4
    - uteis.realizar_operacao: função que executa e exibe as operações
    - Evita repetição de código e melhora a legibilidade do programa

--- calculadora.py
+++ calculadora.py
@@ -8,42 +8,17 @@ while True:
     uteis.menu_opcoes()
     opcao = input()
 
-    if opcao == '1' or opcao == '2':
-        try:
-            lista_numeros = uteis.solicitar_numeros()
-        except ValueError as e:
-            print("Erro: ", e)
-            continue
-        if opcao == '1':
-            resultado = operacoes.soma(lista_numeros)
-            operacao_realizada = uteis.historico_nova_operacao(lista_numeros, '+', resultado)
-            uteis.exibir_operacao(operacao_realizada)
-        elif opcao == '2':
-            resultado = operacoes.subtracao(lista_numeros)
-            operacao_realizada = uteis.historico_nova_operacao(lista_numeros, '-', resultado)
-            uteis.exibir_operacao(operacao_realizada)
+    if opcao == '1':
+        uteis.realizar_operacao(operacoes.soma, '+')
+
+    elif opcao == '2':
+        uteis.realizar_operacao(operacoes.subtracao, '-')
 
     elif opcao == '3':
-        try:
-            lista_numeros = uteis.solicitar_numeros('3')
-        except ValueError as erro:
-            print(f"ERRO: {erro}")
-            continue
-        resultado = operacoes.divisao(lista_numeros)
-        operacao_realizada = uteis.historico_nova_operacao(lista_numeros, '/', resultado)
-        uteis.exibir_operacao(operacao_realizada)
-        uteis.salvar_historico(operacao_realizada)
+        uteis.realizar_operacao(operacoes.divisao, '/')
 
     elif opcao == '4':
-        try:
-            lista_numeros = uteis.solicitar_numeros()
-        except ValueError as erro:
-            print(f"ERRO: {erro}")
-            continue
-        resultado = operacoes.multiplicacao(lista_numeros)
-        operacao_realizada = uteis.historico_nova_operacao(lista_numeros, '*', resultado)
-        uteis.exibir_operacao(operacao_realizada)
-        uteis.salvar_historico(operacao_realizada)
+        uteis.realizar_operacao(operacoes.multiplicacao, '*')
     
     elif opcao == '5':
         uteis.ver_historico()
