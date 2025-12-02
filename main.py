ec848f001f3e6145163294c011c7106b71e352a1
Kaua Santos <kavillykaua@gmail.com>
2025-12-02 09:31:41 -0300

    refactor: renomeia calculadora.py para main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+
+import uteis
+import operacoes
+
+print("\n===== MENU DE OPÇÕES =====")
+while True:
+    uteis.menu_opcoes()
+    opcao = input("SELECIONE UMA OPÇÃO ACIMA (digite o número da opção): ")
+
+    if opcao == '1':
+        uteis.realizar_operacao(operacoes.soma, '+')
+
+    elif opcao == '2':
+        uteis.realizar_operacao(operacoes.subtracao, '-')
+
+    elif opcao == '3':
+        uteis.realizar_operacao(operacoes.divisao, '/')
+
+    elif opcao == '4':
+        uteis.realizar_operacao(operacoes.multiplicacao, '*')
+    
+    elif opcao == '5':
+        uteis.ver_historico()
+
+    elif opcao == '6':
+        uteis.apagar_historico()
+
+    elif opcao == '7':
+        print("Encerrando programa...")
+        print("PROGRAMA ENCERRADO.")
+        sys.exit(0)
+    
+    else:
+        print("OPÇÃO INVÁLIDA! POR FAVOR, SELECIONE UMA OPÇÃO VÁLIDA.\n")
