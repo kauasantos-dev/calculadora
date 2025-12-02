2cbd56ddb2f7e07668da03bb18763adda3e61e8b
Kaua Santos <kavillykaua@gmail.com>
2025-12-02 18:44:06 -0300

    feat: adiciona main guard para executar o programa apenas quando o módulo for chamado diretamente

--- main.py
+++ main.py
@@ -3,33 +3,35 @@ import sys
 from cfgcalculadora import uteis
 from cfgcalculadora import operacoes
 
-print("\n===== MENU DE OPÇÕES =====")
-while True:
-    uteis.menu_opcoes()
-    opcao = input("SELECIONE UMA OPÇÃO ACIMA (digite o número da opção): ")
+if __name__ == '__main__':
 
-    if opcao == '1':
-        uteis.realizar_operacao(operacoes.soma, '+')
+    print("\n===== MENU DE OPÇÕES =====")
+    while True:
+        uteis.menu_opcoes()
+        opcao = input("SELECIONE UMA OPÇÃO ACIMA (digite o número da opção): ")
 
-    elif opcao == '2':
-        uteis.realizar_operacao(operacoes.subtracao, '-')
+        if opcao == '1':
+            uteis.realizar_operacao(operacoes.soma, '+')
 
-    elif opcao == '3':
-        uteis.realizar_operacao(operacoes.divisao, '/')
+        elif opcao == '2':
+            uteis.realizar_operacao(operacoes.subtracao, '-')
 
-    elif opcao == '4':
-        uteis.realizar_operacao(operacoes.multiplicacao, '*')
+        elif opcao == '3':
+            uteis.realizar_operacao(operacoes.divisao, '/')
+
+        elif opcao == '4':
+            uteis.realizar_operacao(operacoes.multiplicacao, '*')
     
-    elif opcao == '5':
-        uteis.ver_historico()
+        elif opcao == '5':
+            uteis.ver_historico()
 
-    elif opcao == '6':
-        uteis.apagar_historico()
+        elif opcao == '6':
+            uteis.apagar_historico()
 
-    elif opcao == '7':
-        print("Encerrando programa...")
-        print("PROGRAMA ENCERRADO.")
-        sys.exit(0)
+        elif opcao == '7':
+            print("Encerrando programa...")
+            print("PROGRAMA ENCERRADO.")
+            sys.exit(0)
     
-    else:
-        print("OPÇÃO INVÁLIDA! POR FAVOR, SELECIONE UMA OPÇÃO VÁLIDA.\n")
+        else:
+            print("OPÇÃO INVÁLIDA! POR FAVOR, SELECIONE UMA OPÇÃO VÁLIDA.\n")
