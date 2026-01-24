6e98b3736a1a5f6c70bd1e31da1f3d92525b6ee6
Kaua Santos <kavillykaua@gmail.com>
2026-01-24 20:51:46 -0300

    refactor: organiza display de exibição de resultados

---
+++ interface/display_resultado.py
@@ -0,0 +1,44 @@
+from tkinter import Tk, Frame, StringVar, Label
+
+class DisplayResultadoOperacoes:
+    def __init__(self, aplicacao: Tk):
+        self.aplicacao = aplicacao
+
+    def frame_resultado_operacoes(self):
+        self.frame_resultados = Frame(
+            self.aplicacao, 
+            bg="#222020",
+            highlightbackground="white",
+            highlightthickness=2
+        )
+
+        self.frame_resultados.place(
+            relx=0.1, 
+            rely=0.05,
+            relwidth=0.8, 
+            relheight=0.16
+        )
+
+        self.valor_display = StringVar()
+        self.valor_display.set("0")
+
+        self.display = Label(
+            self.frame_resultados,
+            textvariable=self.valor_display,
+            bg="white",
+            fg="black",
+            anchor="e",
+            font=("Arial", 20)
+        )
+
+        self.display.place(
+            relwidth=1,
+            relheight=1
+        )
+
+    def inserir(self, valor):
+        valor_atual = self.valor_display.get()
+        if valor_atual == "0":
+            self.valor_display.set(str(valor))
+        else:
+            self.valor_display.set(valor_atual + str(valor))
